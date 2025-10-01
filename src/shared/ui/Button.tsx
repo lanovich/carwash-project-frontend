3361e4a30f507695614f3057df093df65c4336e5
lanovich <kolaemelanov60@gmail.com>
2025-10-01 14:12:32 +0300

    feat: add Button component and useMobile hook

---
+++ src/shared/ui/Button.tsx
@@ -0,0 +1,83 @@
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/shared/lib";
+import React from "react";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md transition-all disabled:cursor-not-allowed disabled:opacity-50",
+
+  {
+    variants: {
+      variant: {
+        primary: `
+          bg-primary text-white
+          hover:bg-primary-hover
+          active:bg-primary-active
+          disabled:bg-primary-disabled
+        `,
+        secondary: `
+          bg-secondary text-white
+          hover:bg-secondary-hover
+          active:bg-secondary-active
+          disabled:bg-secondary-disabled
+        `,
+        ghost: `
+          bg-transparent border border-primary text-text-subtle
+          hover:bg-text-subtle/10
+          active:bg-text-subtle/20
+          disabled:opacity-50
+        `,
+      },
+      size: {
+        default: "px-5 py-3 text-regular",
+        sm: "px-5 py-2 text-small",
+        lg: "px-6 py-5 text-lg",
+      },
+      iconOnly: {
+        true: "p-2 aspect-square",
+        false: "",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "sm",
+      iconOnly: false,
+    },
+  }
+);
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  icon?: React.ReactNode;
+  iconPosition?: "left" | "right";
+}
+
+export const Button = ({
+  className,
+  variant,
+  size,
+  icon,
+  iconPosition = "left",
+  asChild = false,
+  children,
+  iconOnly,
+  ...props
+}: ButtonProps) => {
+  const Comp = asChild ? Slot : "button";
+
+  return (
+    <Comp
+      className={cn(buttonVariants({ variant, size, iconOnly, className }))}
+      {...props}
+    >
+
+        {icon && iconPosition === "left" && icon}
+
+        {children}
+
+        {icon && iconPosition === "right" && icon}
+    </Comp>
+  );
+};
