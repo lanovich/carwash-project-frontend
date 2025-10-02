b6129c4852de097a8504858fc274742f831510cf
lanovich <kolaemelanov60@gmail.com>
2025-10-02 23:04:23 +0300

    style: change logo gap and address font-size

--- src/widgets/header/ui/Logo.tsx
+++ src/widgets/header/ui/Logo.tsx
@@ -1,6 +1,6 @@
 export const Logo = () => (
-  <div className="flex items-center gap-2 2xl:gap-4">
+  <div className="flex items-center gap-2">
     <img src="/logo.svg" alt="Logo" width={40} height={40} />
-    <div className="text-h2">АВТОМОЙКА</div>
+    <div className="text-h1">АВТОМОЙКА</div>
   </div>
 );
