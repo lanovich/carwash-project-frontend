b3314f152f4ea34dcd706c9cd9ad33d104308b57
lanovich <kolaemelanov60@gmail.com>
2025-10-13 23:15:14 +0300

    fix: change warning

--- src/entities/booking/ui/BookingSummary.tsx
+++ src/entities/booking/ui/BookingSummary.tsx
@@ -46,7 +46,6 @@ export const BookingSummary = () => {
         <BookingRow size="lg" name="Цена" value={`${totalPrice} ₽`} />
       </InfoBlock>
 
-      <Button className="text-white">Подтвердить и записаться</Button>
       <div className="p-2 bg-yellow-50 border border-yellow-300 rounded-md flex flex-col gap-2">
         <div className="flex items-center gap-2">
           <AlertCircle className="w-5 h-5 text-yellow-600 flex-shrink-0" />
@@ -56,19 +55,20 @@ export const BookingSummary = () => {
         </div>
         <ul className="list-none text-xs text-yellow-800 ml-1 flex flex-col gap-1">
           <li>
-            • Указанные цены соответствуют прайсу, но при сильном загрязнении и
-            большом объёме работ окончательную стоимость озвучивает
-            администратор
+            • Цены ориентировочные и могут быть скорректированы в зависимости от
+            объёма работы или степени загрязнения.
           </li>
           <li>
-            • Услуги, указанные за шт. или кв.м, на месте могут быть пересчитаны
+            • Услуги, указанные за штуку или за кв.м, рассчитываются на месте по
+            фактическому объёму.
           </li>
           <li>
-            • Оператор должен дозвониться до вас для подтверждения заказа, иначе
-            запись не состоится
+            • Оператор свяжется с вами для подтверждения заказа. Без
+            подтверждения запись не состоится.
           </li>
         </ul>
       </div>
+      <Button className="text-white">Подтвердить и записаться</Button>
     </div>
   );
 };
