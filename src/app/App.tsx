e10a7eaa75e59f29605d41170f3c75325bdcd3fa
lanovich <kolaemelanov60@gmail.com>
2026-04-23 22:36:32 +0300

    feat(toast): integrate Sonner toaster globally

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "@/app/layouts";
 import { ROUTES } from "@/shared/lib";
 import { lazy } from "react";
+import { Toaster } from "sonner";
 
 const BookingPage = lazy(() => import("@/pages/booking-page/ui/BookingPage"));
 const MainPage = lazy(() => import("@/pages/main-page/ui/MainPage"));
@@ -9,13 +10,16 @@ const AdminPage = lazy(() => import("@/pages/admin-page/ui/AdminPage"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<BookingPage />} />
-        <Route path={ROUTES.home} element={<MainPage />} />
-        <Route path={ROUTES.admin} element={<AdminPage />} />
-      </Route>
-    </Routes>
+    <>
+      <Toaster position="top-center" richColors />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<BookingPage />} />
+          <Route path={ROUTES.home} element={<MainPage />} />
+          <Route path={ROUTES.admin} element={<AdminPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
